2002dc9cae7c90fdc8658554399e8967be9ab3a8
Orzechowski Gracjan <orzechowski.gracjan@gmail.com>
2018-10-16 15:55:58 +0200

    added serializer example

---
+++ examples/serializer.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restly import FlaskRestly
+from flask_restly.decorator import resource, get
+
+
+app = Flask(__name__)
+
+# json is default serializer
+# from flask_restly.serializer import json
+# app.config['RESTLY_DEFAULT_SERIALIZER'] = json
+
+rest = FlaskRestly(app)
+rest.init_app(app)
+
+
+@resource(name='employees')
+class EmployeesResource:
+    @get('/<id>', serializer=lambda result: str(result.get('id')))
+    def get_employee(self, id):
+        return dict(id=int(id))
+
+    @get('/')
+    def get_employees(self):
+        return dict(entites=[
+            dict(id=1),
+            dict(id=2)
+        ])
+
+
+with app.app_context():
+    EmployeesResource()
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5001, debug=True)
