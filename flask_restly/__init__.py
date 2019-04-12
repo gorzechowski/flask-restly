6aed6cf588ddd3ee555c71d444b30fbe091996b9
Orzechowski Gracjan <orzechowski.gracjan@gmail.com>
2019-04-12 15:14:14 +0200

    initial rating limit decorators

--- flask_restly/__init__.py
+++ flask_restly/__init__.py
@@ -29,6 +29,33 @@ def api_error_handler(error):
     return _jsonify_error(InternalServerError()), 500
 
 
+def _inject_rate_limit_headers(response):
+    try:
+        requests, remaining, reset = request.rate_limit_headers
+    except (AttributeError, ValueError):
+        return response
+    else:
+        h = response.headers
+        h.add('X-RateLimit-Limit', requests)
+        h.add('X-RateLimit-Remaining', remaining)
+        h.add('X-RateLimit-Reset', reset)
+
+        return response
+
+
+def _default_key_resolver(group, _):
+    if group is None:
+        group = request.endpoint
+
+    headers = request.headers.getlist('X-Forwarded-For')
+    if headers:
+        ip = headers[0]
+    else:
+        ip = request.remote_addr
+
+    return "__".join([ip, group])
+
+
 class FlaskRestly(object):
     _app = None
     _error_handler = None
@@ -56,11 +83,16 @@ class FlaskRestly(object):
         app.config.setdefault('RESTLY_SERIALIZER', json)
         app.config.setdefault('RESTLY_API_PREFIX', '/api/rest')
         app.config.setdefault('RESTLY_PROTOBUF_MIMETYPE', 'application/x-protobuf')
+        app.config.setdefault('RESTLY_RATE_LIMIT_REQUESTS_AMOUNT', 100)
+        app.config.setdefault('RESTLY_RATE_LIMIT_WINDOW_SECONDS', 60)
+        app.config.setdefault('RESTLY_RATE_LIMIT_KEY_RESOLVER', _default_key_resolver)
 
         if self._error_handler is not None:
             app.register_error_handler(Exception, self._error_handler)
         else:
             app.register_error_handler(Exception, api_error_handler)
+
+        app.after_request(_inject_rate_limit_headers)
 
         self._app = app
 
--- flask_restly/_view.py
+++ flask_restly/_view.py
@@ -4,7 +4,7 @@ from flask import (
     request,
 )
 from flask_restly._storage import get_metadata_storage
-from flask_restly.exception import Forbidden, BadRequest
+from flask_restly.exception import Forbidden, BadRequest, TooManyRequests
 
 
 def _view_factory(instance, obj, callback, serialize):
@@ -23,6 +23,9 @@ def _view_factory(instance, obj, callback, serialize):
     queued = metadata.get('queued', False)
     auth_provider = get_metadata_storage().get('auth_provider', lambda: True)
     identity_provider = get_metadata_storage().get('identity_provider', None)
+    rate_limit_resolver = get_metadata_storage().get('rate_limit_resolver', lambda: None)
+    key_resolver = current_app.config.get('RESTLY_RATE_LIMIT_KEY_RESOLVER')
+    rating_limit = metadata.get('rating_limit', None)
 
     def wrapper(*args, **kwargs):
         if not skip_auth and auth_provider() is False:
@@ -30,6 +33,22 @@ def _view_factory(instance, obj, callback, serialize):
 
         if inject_identity and identity_provider is not None:
             kwargs['identity'] = identity_provider()
+
+        if rating_limit and rate_limit_resolver is not None:
+            group = rating_limit.get('group', None)
+            requests_limit = rating_limit.get('requests', current_app.config.get('RESTLY_RATE_LIMIT_REQUESTS_AMOUNT'))
+            window = rating_limit.get('window', current_app.config.get('RESTLY_RATE_LIMIT_WINDOW_SECONDS'))
+
+            key = key_resolver(group, kwargs.get('identity', None))
+
+            requests_amount, valid_to = rate_limit_resolver(key, window, kwargs.get('identity', None))
+
+            remaining = requests_limit - requests_amount
+
+            if remaining < 0:
+                raise TooManyRequests()
+
+            request.rate_limit_headers = (requests_limit, remaining, int(valid_to))
 
         if inject_body:
             if len(request.get_data()) == 0:
